517a265ef2efad529bbdeb4d42180e188554170d
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-06 11:18:16 +0530

    automate commit

--- app.py
+++ app.py
@@ -1,63 +1,71 @@
 from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-class UserAPI:
-    def __init__(self, uri, database_name="rajesh", collection_name="rajesh"):
-        self.app = Flask(__name__)
-        self.client = MongoClient(uri, server_api=ServerApi('1'))
-        self.db = self.client[database_name]
-        self.collection = self.db[collection_name]
+app = Flask(__name__)
+api = Api(app)
 
-        @self.app.route('/')
-        def message():
-            return "<html><body><h1>Hi, welcome to the RestApi built using Flask. You are currently at the root URL</h1></body></html>"
+# mongodb connection url
+uri = "mongodb+srv://hasura:RsEB60OKT1ON0veq@cluster0.wg3z167.mongodb.net/?retryWrites=true&w=majority"
 
-        @self.app.route('/users')
-        def users():
-            return str(list(self.collection.find()))
+# database name and collection name
+database_name = "rajesh"
+collection_name = "rajesh"
 
-        @self.app.route('/users/<int:user_id>')
-        def get_user(user_id):
-            return str(self.collection.find_one({"id": user_id}))
+# connect to mongodb
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client[database_name]
+collection = db[collection_name]
 
-        @self.app.route('/users', methods=['POST'])
-        def create_user():
-            data = request.json
-            if data:
-                user_dict = {"id": data.get("id"),
-                             "name": data.get("name"),
-                             "email": data.get("email"),
-                             "password": data.get("password")}
-                result = self.collection.insert_one(user_dict)
-                if result.inserted_id:
-                    return jsonify({"message": "User inserted successfully"}), 201
-                else:
-                    return jsonify({"error": "Invalid data"}), 400
+# create a class for the resource
+class UsersResource(Resource):
+    def get(self):
+        documents = list(collection.find())
+        for document in documents:
+            document["_id"] = str(document["_id"])
 
-        @self.app.route('/users/<int:id>', methods=["POST"])
-        def update_user(id):
-            data = request.json
-            if data:
-                user_dict = {"id": data.get("id"),
-                             "name": data.get("name"),
-                             "email": data.get("email"),
-                             "password": data.get("password")}
-                result = self.collection.update_one({"id": id}, {"$set": user_dict})
-                if result.modified_count:
-                    return jsonify({"message": "User updated successfully"}), 200
-                else:
-                    return jsonify({"error": "Invalid data"}), 400
+        return jsonify(documents)
 
-        @self.app.route('/users/<int:id>', methods=["DELETE"])
-        def delete_user(id):
-            result = self.collection.delete_one({"id": id})
-            if result.deleted_count:
-                return jsonify({"message": "User deleted successfully"}), 200
-            else:
-                return jsonify({"error": "User not found"}), 404
-            
+    def post(self):
+        data = request.json
+        user_dict = {"id": data.get("id"),
+                     "name": data.get("name"),
+                     "email": data.get("email"),
+                     "password": data.get("password")}
+        result = collection.insert_one(user_dict)
+        if result.inserted_id:
+            return jsonify({"message": "User added successfully"})
+        else:
+            return jsonify({"error": "Invalid data"})
+
+# create a class for the resource
+class UserResource(Resource):
+    def get(self, id):
+        return jsonify(collection.find_one({"id": id}))
+
+    def put(self, id):
+        data = request.json
+        user_dict = {"id": data.get("id"),
+                     "name": data.get("name"),
+                     "email": data.get("email"),
+                     "password": data.get("password")}
+        result = collection.update_one({"id": id}, {"$set": user_dict})
+        if result.inserted_id:
+            return jsonify({"message": "User data updated successfully"})
+        else:
+            return jsonify({"error": "Invalid data"})
+
+    def delete(self, id):
+        result = collection.delete_one({"id": id})
+        if result.inserted_id:
+            return jsonify({"message": "User deleted successfully"})
+        else:
+            return jsonify({"error": "Invalid data"})
+
+# add the resource to the api
+api.add_resource(UsersResource, '/users')
+api.add_resource(UserResource, '/users/<int:id>')
+
 if __name__ == '__main__':
-    uri = "mongodb+srv://hasura:RsEB60OKT1ON0veq@cluster0.wg3z167.mongodb.net/?retryWrites=true&w=majority"
-    user_api = UserAPI(uri)
-    user_api.app.run(host='0.0.0.0', port=5000,debug=False)
+    app.run(host='0.0.0.0', port=5000,debug=False)
