09a75ca8d8f24f16b0e0df6f76e2d7f980943033
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-06 10:03:28 +0530

    automate commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11.0a1-alpine3.14
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 80
+
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,90 +1,63 @@
-# Importing the flask module in the project is mandatory.
-from flask import *  
-
-# Flask constructor takes the name of current module (__name__) as argument.
-app = Flask(__name__)
-
-# Import MongoClient from pymongo so we can connect to the database
+from flask import Flask, request, jsonify
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-# Create a URI to connect to MongoDB Atlas
-uri = "mongodb+srv://hasura:RsEB60OKT1ON0veq@cluster0.wg3z167.mongodb.net/?retryWrites=true&w=majority"
+class UserAPI:
+    def __init__(self, uri, database_name="rajesh", collection_name="rajesh"):
+        self.app = Flask(__name__)
+        self.client = MongoClient(uri, server_api=ServerApi('1'))
+        self.db = self.client[database_name]
+        self.collection = self.db[collection_name]
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+        @self.app.route('/')
+        def message():
+            return "<html><body><h1>Hi, welcome to the RestApi built using Flask. You are currently at the root URL</h1></body></html>"
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-    db = client['rajesh']
-    collection = db['rajesh']
+        @self.app.route('/users')
+        def users():
+            return str(list(self.collection.find()))
 
-# If an exception is raised, then the server is not available
-except Exception as e:
-    print(e)
+        @self.app.route('/users/<int:user_id>')
+        def get_user(user_id):
+            return str(self.collection.find_one({"id": user_id}))
 
-# A decorator used to tell the application
-@app.route('/')  
-def message():  
-      return "<html><body><h1>Hi, welcome to the RestApi build using flask you are currently at the root url</h1></body></html>"
+        @self.app.route('/users', methods=['POST'])
+        def create_user():
+            data = request.json
+            if data:
+                user_dict = {"id": data.get("id"),
+                             "name": data.get("name"),
+                             "email": data.get("email"),
+                             "password": data.get("password")}
+                result = self.collection.insert_one(user_dict)
+                if result.inserted_id:
+                    return jsonify({"message": "User inserted successfully"}), 201
+                else:
+                    return jsonify({"error": "Invalid data"}), 400
 
-# returning all the users from the mongodb database
-@app.route('/users')
-def users():
-    # return all the documents in the 'users' collection
-    return str(list(collection.find()))
+        @self.app.route('/users/<int:id>', methods=["POST"])
+        def update_user(id):
+            data = request.json
+            if data:
+                user_dict = {"id": data.get("id"),
+                             "name": data.get("name"),
+                             "email": data.get("email"),
+                             "password": data.get("password")}
+                result = self.collection.update_one({"id": id}, {"$set": user_dict})
+                if result.modified_count:
+                    return jsonify({"message": "User updated successfully"}), 200
+                else:
+                    return jsonify({"error": "Invalid data"}), 400
 
-# returning the user with the given user_id from the mongodb database
-@app.route('/users/<int:user_id>')
-def get_user(user_id):
-    # return the user with the given user_id
-    return str(collection.find_one({"id": user_id}))
-
-# creating a new user in the mongodb database
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.json  # Assuming you are sending JSON data in the request
-    if data:
-        user_dict = {"id": data.get("id"),
-                     "name": data.get("name"),
-                     "email": data.get("email"),
-                     "password": data.get("password")}
-        result = collection.insert_one(user_dict)
-    if result.inserted_id:
-        return jsonify({"message": "User inserted successfully"}), 201
-    else:
-        return jsonify({"error": "Invalid data"}), 400  # Bad Request
-
-# updating the user with the given user_id in the mongodb database
-@app.route('/users/<int:id>',methods=["POST"])
-def update_user(id):
-    data = request.json  # Assuming you are sending JSON data in the request
-    if data:
-        user_dict = {"id": data.get("id"),
-                     "name": data.get("name"),
-                     "email": data.get("email"),
-                     "password": data.get("password")}
-        
-        result = collection.update_one({"id": id}, {"$set": user_dict})
-
-    if result.modified_count:
-        return jsonify({"message": "User updated successfully"}), 200
-    else:
-        return jsonify({"error": "Invalid data"}), 400
-
-# deleting the user with the given user_id in the mongodb database
-@app.route('/users/<int:id>',methods=["DELETE"])
-def delete_user(id):
-    # delete the user with the given user_id
-    result = collection.delete_one({"id": id})
-    if result.deleted_count:
-        return jsonify({"message": "User deleted successfully"}), 200
-    else:
-        return jsonify({"error": "User not found"}), 404
-
-# main driver function
+        @self.app.route('/users/<int:id>', methods=["DELETE"])
+        def delete_user(id):
+            result = self.collection.delete_one({"id": id})
+            if result.deleted_count:
+                return jsonify({"message": "User deleted successfully"}), 200
+            else:
+                return jsonify({"error": "User not found"}), 404
+            
 if __name__ == '__main__':
-    # run() method of Flask class runs the application on the local development server. Debug is set to false in production mode but keep it to true in development mode
-   app.run(debug = False)#    
+    uri = ""
+    user_api = UserAPI(uri)
+    user_api.app.run(debug=False)
